80b4b19f53a3408caf699811731ce5c23c8cd37c
cheininger <christoph_heininger@hotmail.com>
2023-10-09 11:33:16 +0200

    added display and some button functionality

--- js/index.js
+++ js/index.js
@@ -1,11 +1,51 @@
-let firstInput;
-let secondInput;
-let operator;
+let firstInput = "";
+let secondInput = "";
+let operator = "";
 
 const addButton = document.querySelector("button.add");
 const subtractButton = document.querySelector("button.subtract");
 const multiplyButton = document.querySelector("button.multiply");
 const divideButton = document.querySelector("button.divide");
+const equalsButton = document.querySelector("button.equals");
+const decimalButton = document.querySelector("button.decimal");
+const percentButton = document.querySelector("button.percent");
+const numberButtons = document.querySelectorAll("button.number");
+const clearButton = document.querySelector("button.clear");
+const clearAllButton = document.querySelector("button.clear-all");
+const display = document.querySelector(".display");
+
+addButton.addEventListener("click", () => {
+  operator = "+";
+  setDisplay(operator);
+});
+subtractButton.addEventListener("click", () => {
+  operator = "-";
+  setDisplay(operator);
+});
+multiplyButton.addEventListener("click", () => {
+  operator = "*";
+  setDisplay(operator);
+});
+divideButton.addEventListener("click", () => {
+  operator = "/";
+  setDisplay(operator);
+});
+
+for (let i = 0; i < numberButtons.length; i++) {
+  numberButtons[i].addEventListener("click", () => {
+    if (operator === "") {
+      firstInput += numberButtons[i].textContent;
+      setDisplay(firstInput);
+    } else {
+      secondInput += numberButtons[i].textContent;
+      setDisplay(secondInput);
+    }
+  });
+}
+
+function setDisplay(string) {
+  display.textContent = string;
+}
 
 function add(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
